dd6656f6f630ecc50dc9805f652080fb3f473cc2
MesopotamiaAlpha <leao-gomes@hotmail.com>
2017-01-08 00:32:00 -0300

    Update iniciarservidor.sh

--- iniciarservidor.sh
+++ iniciarservidor.sh
@@ -1,8 +1,9 @@
+
 #!/bin/sh
-#este script é para iniciar o ts dentro do servido e conferir se o processo esta rodando,adicionei dentro do /etc/profile
-cd /home/jose/teamspeak3-server_linux_x86/
-./ts3server_minimal_runscript.sh filetransfer_ip=0.0.0.0 filetransfer_port=30035 #aqui deve se colocar a porta desejada
-        pgrep ts3server && echo '\033[32mRodando\033[0m'
-        pgrep ts3server || echo '\033[1;31mNao esta rodando\033[0m'
+#este script é para iniciar o ts dentro do servidor e conferir se o processo esta rodando
+cd /home/usuario/teamspeak3-server_linux_x86/
+./ts3server_minimal_runscript.sh filetransfer_ip=0.0.0.0 filetransfer_port=30035 query_ip=0.0.0.0 query_port=10015 #aqui deve se colocar a porta desejada
+        pgrep ts3server && echo '\033[32mO TeamSpeak foi iniciado corretamente\033[0m'
+        pgrep ts3server || echo '\033[1;31mO TeamSpeak nao foi iniciado\033[0m'
 
 exit 0;
